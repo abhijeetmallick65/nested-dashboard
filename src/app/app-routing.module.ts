1dd51c862824a38ebc2410f5f378b11d5874c35e
Abhijeet <abhijeetmallick29@gmail.com>
2025-04-19 15:06:30 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HttpClientModule} from '@angular/common/http';
 import { NestedDashboardComponent } from './nested-dashboard/nested-dashboard.component';
 
 const routes:Routes=[
-  {path:'',redirectTo:'nested',pathMatch:'full'},
-  { path: 'nested', component: NestedDashboardComponent },
-  {path:'dashboard',loadChildren:()=>import('./dashboard/dashboard.module').then(m=>m.DashboardModule)}
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {path:'dashboard',loadChildren:()=>import('./dashboard/dashboard.module').then(m=>m.DashboardModule)},
+  { path: 'nested', component: NestedDashboardComponent }
 
 ]
 @NgModule({
